34c4b7ea48b836639d758a35a52453d95c599ffe
MUHAMAD ABDUL ANAS <115569493+muhamadabdulanas@users.noreply.github.com>
2025-01-05 06:32:01 +0700

    Add files via upload

---
+++ ttdBarcode.py
@@ -0,0 +1,12 @@
+import barcode
+from barcode.writer import ImageWriter
+from PIL import Image
+
+nm = ""
+gambar = " https://muhamadabdulanas.github.io/github..io/"
+
+ean = barcode.get('code128', gambar, writer=ImageWriter())
+nama = input("Masukkan nama barcode")
+ean.save(nama)
+ean = Image.open(nama + ".png")
+ean.show()
